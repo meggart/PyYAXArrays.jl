8b9a775b3d492d4bbe42c5d677678c43ede89d9d
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-08-26 14:19:47 -0700

    Add and correct Python benchmarks

--- benchmarks/chunked_reads.py
+++ benchmarks/chunked_reads.py
@@ -13,17 +13,17 @@ v = dataset["v"]
 single_chunk_read_time = timeit.timeit(
     stmt = "output_storage[:, :, :] = v[0:(v.chunks[0][0]), 0:(v.chunks[1][0]), 0:(v.chunks[2][0])]",
     globals = {"v": v, "output_storage": np.zeros((v.chunks[0][0], v.chunks[1][0], v.chunks[2][0]))},
-    number = 10,
-)
+    number = 50,
+) / 50
 
 hundred_chunk_contiguous_read_time = timeit.timeit(
     stmt = "output_storage[:, :] = v[:, :, 0]",
     globals = {"v": v, "output_storage": np.zeros((sum(v.chunks[0]), sum(v.chunks[1])))},
     number = 10,
-)
+) / 10
 
 random_access_time = timeit.timeit(
     stmt = "v[x, y, z]",
-    globals = {"v": v, "x": np.random.randint(0, v.chunks[0][0]), "y": np.random.randint(0, v.chunks[1][0]), "z": np.random.randint(0, v.chunks[2][0])},
+    globals = {"v": v, "x": np.random.randint(0, sum(v.chunks[0]), 1000), "y": np.random.randint(0, sum(v.chunks[1]), 1000), "z": np.random.randint(0, sum(v.chunks[2]), 1000)},
     number = 10,
-)
+) / 10
